e315258990b54b26d5df0d830c1c3afadfc37493
Saler C. Lourenço <saler.cez@gmail.com>
2023-11-09 19:45:29 -0300

    initial commit

---
+++ src/projeto2saler/Projeto2SALER.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projeto2saler;
+
+/**
+ *
+ * @author saler
+ */
+public class Projeto2SALER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
