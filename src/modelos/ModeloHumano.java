d41aa76cf69a0dc825aab65d5293821475af0bb6
Saler C. Lourenço <saler.cez@gmail.com>
2023-11-22 01:48:26 -0300

    finaliza modelos

--- src/modelos/ModeloHumano.java
+++ src/modelos/ModeloHumano.java
@@ -1,13 +1,81 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelos;
 
-/**
- *
- * @author saler
- */
 public class ModeloHumano {
-    
+
+    int idHumano;
+    String nomeHumano;
+    int idSexo;
+    int idRotulo;
+    String sexo;
+    String rotulo;
+    String fotoHumano;
+
+    public ModeloHumano() {
+    }
+
+    public ModeloHumano(int idHumano, String nomeHumano, int idSexo, int idRotulo, String sexo, String rotulo, String fotoHumano) {
+        this.idHumano = idHumano;
+        this.nomeHumano = nomeHumano;
+        this.idSexo = idSexo;
+        this.idRotulo = idRotulo;
+        this.sexo = sexo;
+        this.rotulo = rotulo;
+        this.fotoHumano = fotoHumano;
+    }
+
+    public int getIdHumano() {
+        return this.idHumano;
+    }
+
+    public void setIdHumano(int idHumano) {
+        this.idHumano = idHumano;
+    }
+
+    public String getNomeHumano() {
+        return this.nomeHumano;
+    }
+
+    public void setNomeHumano(String nomeHumano) {
+        this.nomeHumano = nomeHumano;
+    }
+
+    public int getIdSexo() {
+        return this.idSexo;
+    }
+
+    public void setIdSexo(int idSexo) {
+        this.idSexo = idSexo;
+    }
+
+    public int getIdRotulo() {
+        return this.idRotulo;
+    }
+
+    public void setIdRotulo(int idRotulo) {
+        this.idRotulo = idRotulo;
+    }
+
+    public String getSexo() {
+        return this.sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getRotulo() {
+        return this.rotulo;
+    }
+
+    public void setRotulo(String rotulo) {
+        this.rotulo = rotulo;
+    }
+
+    public String getFotoHumano() {
+        return this.fotoHumano;
+    }
+
+    public void setFotoHumano(String fotoHumano) {
+        this.fotoHumano = fotoHumano;
+    }
 }
--- src/modelos/ModeloLog.java
+++ src/modelos/ModeloLog.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonObject;
 import java.security.Timestamp;
 
 public class ModeloLog {
+
     int idLog;
     Timestamp datahora;
     String usuario;
@@ -17,16 +18,79 @@ public class ModeloLog {
     public ModeloLog() {
     }
 
-    public ModeloLog(int idLog, Timestamp datahora, String usuario, String ip, String depoisString, JsonObject depoisJson, String tabelaEditada, String comando) {
+    public ModeloLog(int idLog, Timestamp datahora, String usuario, String ip, String depoisString, String tabelaEditada, String comando) {
         this.idLog = idLog;
         this.datahora = datahora;
         this.usuario = usuario;
         this.ip = ip;
         this.depoisString = depoisString;
-        this.depoisJson = depoisJson;
         this.tabelaEditada = tabelaEditada;
         this.comando = comando;
+        Gson gson = new Gson();
+        this.depoisJson = gson.fromJson(depoisString, JsonObject.class);
     }
-    
-    
+
+    public int getIdLog() {
+        return this.idLog;
+    }
+
+    public void setIdLog(int idLog) {
+        this.idLog = idLog;
+    }
+
+    public Timestamp getDatahora() {
+        return this.datahora;
+    }
+
+    public void setDatahora(Timestamp datahora) {
+        this.datahora = datahora;
+    }
+
+    public String getUsuario() {
+        return this.usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getIp() {
+        return this.ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getDepoisString() {
+        return this.depoisString;
+    }
+
+    public void setDepoisString(String depoisString) {
+        this.depoisString = depoisString;
+    }
+
+    public JsonObject getDepoisJson() {
+        return this.depoisJson;
+    }
+
+    public void setDepoisJson(JsonObject depoisJson) {
+        this.depoisJson = depoisJson;
+    }
+
+    public String getTabelaEditada() {
+        return this.tabelaEditada;
+    }
+
+    public void setTabelaEditada(String tabelaEditada) {
+        this.tabelaEditada = tabelaEditada;
+    }
+
+    public String getComando() {
+        return this.comando;
+    }
+
+    public void setComando(String comando) {
+        this.comando = comando;
+    }
 }
